7833cb93316421649372773e57182d242007a54d
thallyslemos <thallysvilemos@gmail.com>
2023-06-15 01:39:51 -0300

    falta pouco

--- src/App.java
+++ src/App.java
@@ -2,12 +2,165 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class App {
+
+    public static void menuPrincipal() {
+        System.out.println("-------- Menu --------\n");
+        System.out.println("(1) - Pilha de revistas");
+        System.out.println("(2) - Vendas");
+        System.out.println("(3) - Limpar terminal");
+        System.out.println("(0) - Sair\n");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    public static void menuRevistas() {
+        System.out.println("------- Pilha de Revistas -------\n");
+        System.out.println("(1) - Listar a pilha de revistas");
+        System.out.println("(2) - Incluir nova revista");
+        System.out.println("(3) - Limpar terminal");
+        System.out.println("(0) - Voltar ao menu principal\n");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    public static void menuVenda() {
+        System.out.println("--------- Vendas ---------\n");
+        System.out.println("(1) - Listar todas as vendas");
+        System.out.println("(2) - Incluir nova venda");
+        System.out.println("(3) - Limpar terminal");
+        System.out.println("(0) - Voltar ao menu principal\n");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    public static void cadastraRevista(Scanner scanner, Stack<Revista> pilha_revistas) {
+        System.out.println("Digite o título: ");
+        String titulo = scanner.next();
+        System.out.println("Digite número de edição: ");
+        int edicao = scanner.nextInt();
+        System.out.println("Digite ano de publicação: ");
+        int ano = scanner.nextInt();
+        System.out.println("Digite o mês de publicação: ");
+        int mes = scanner.nextInt();
+        System.out.println("Digite o número de volume (caso não exista, digite 0): ");
+        int volume = scanner.nextInt();
+
+        // Inserir código que cadastra a revista
+        if (volume == 0) {
+            pilha_revistas.add(new Revista(titulo, edicao, mes, ano));
+        } else {
+            pilha_revistas.add(new Revista(titulo, edicao, mes, ano, volume));
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-         // Lista para manipulação dos itens da compra
-         List<Mercadoria> lista_compra = new ArrayList<>();
-         // Implementação de pilha de revistas com arrayDeque
-        Queue<Revista> pilha_revistas = new ArrayDeque<>();
+        Scanner scanner = new Scanner(System.in);
+        // Lista para manipulação dos itens da compra
+        List<Mercadoria> lista_compra = new ArrayList<>();
+        // Implementação de pilha de revistas com arrayDeque
+        Stack<Revista> pilha_revistas = new Stack<>();
+        int menu = 1;
+
+        while (menu != 0) {
+            // System.out.print("\033[H\033[2J");
+            menuPrincipal();
+            menu = scanner.nextInt();
+
+            switch (menu) {
+
+                case 1:
+                    int segundo_menu = 1;
+
+                    while (segundo_menu != 0) {
+                        // System.out.print("\033[H\033[2J");
+                        menuRevistas();
+
+                        segundo_menu = scanner.nextInt();
+
+                        switch (segundo_menu) {
+                            case 1:
+                                System.out.println("Impressao de revistas");
+                                for (Revista r : pilha_revistas) {
+                                    System.out.println(r.toString());
+                                }
+                                break;
+                            case 2:
+                                // System.out.print("\033[H\033[2J");
+                                cadastraRevista(scanner, pilha_revistas);
+                                break;
+                            case 3:
+                                System.out.print("\033[H\033[2J");
+                                break;
+                            case 0:
+                                break;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    int terceiro_menu = 1;
+
+                    while (terceiro_menu != 0) {
+                        // System.out.print("\033[H\033[2J");
+                        menuVenda();
+                        terceiro_menu = scanner.nextInt();
+
+                        switch (terceiro_menu) {
+                            case 1:
+                                System.out.println("Listar vendas");
+                                break;
+                            case 2:
+                                int quarto_menu = 1;
+                                while (quarto_menu != 0) {
+                                    // System.out.print("\033[H\033[2J");
+                                    // adcionar o número da venda
+                                    System.out.println("Nº da venda: ");
+                                    System.out.println("-----------------------------------------\n");
+                                    System.out.println("Itens: ");
+                                    // Adcionar itens
+                                    System.out.println("Valor Total: "); // Adcionar valor total
+                                    System.out.println("\n-----------------------------------------");
+                                    System.out.println("(1) - Incluir novo item");
+                                    System.out.println("(2) - Excluir item");
+                                    System.out.println("(0) - Finalizar venda\n");
+
+                                    System.out.print("Escolha uma opção: ");
+                                    quarto_menu = scanner.nextInt();
+
+                                    switch (quarto_menu) {
+                                        case 1:
+                                            break;
+                                        case 3:
+                                            System.out.print("\033[H\033[2J");
+                                            break;
+                                        case 0:
+                                            break;
+                                    }
+                                    ;
+                                }
+
+                                break;
+                            case 3:
+                                System.out.print("\033[H\033[2J");
+                                break;
+                            case 0:
+                                // Voltar para o menu
+                                break;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("\033[H\033[2J");
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+                    break;
+            }
+        }
     }
 }
--- src/Revista.java
+++ src/Revista.java
@@ -1,34 +1,41 @@
+import java.util.Date;
+
 public class Revista {
     private String titulo;
     private int numero_edicao;
     private int mes_publicacao;
     private int ano_publicacao;
     private int numero_volume;
+    private Date data_cadastro;
+    private Date data_retirada;
 
     // Construtor para revistas com volume
     public Revista(String titulo,
-    int numero_edicao,
-    int mes_publicacao,
-    int ano_publicacao,
-    int numero_volume) {
+            int numero_edicao,
+            int mes_publicacao,
+            int ano_publicacao,
+            int numero_volume) {
         // validacao de mes e ano de publicacao
         this.titulo = titulo;
         this.numero_edicao = numero_edicao;
         this.mes_publicacao = mes_publicacao;
         this.ano_publicacao = ano_publicacao;
         this.numero_volume = numero_volume;
+        this.data_cadastro = new Date();
     }
-    
+
     // Construtor para revistas sem volume
     public Revista(String titulo,
-    int numero_edicao,
-    int mes_publicacao,
-    int ano_publicacao) {
+            int numero_edicao,
+            int mes_publicacao,
+            int ano_publicacao) {
         // validacao de mes e ano de publicacao
         this.titulo = titulo;
         this.numero_edicao = numero_edicao;
         this.mes_publicacao = mes_publicacao;
         this.ano_publicacao = ano_publicacao;
+        this.data_cadastro = new Date();
+
     }
 
     public int getAno_publicacao() {
@@ -50,12 +57,27 @@ public class Revista {
     public int getNumero_volume() {
         return numero_volume;
     }
+    public void setData_retirada(Date data_retirada) {
+        this.data_retirada = data_retirada;
+    }
+
+    public Date getData_cadastro() {
+        return data_cadastro;
+    }
+
+    public Date getData_retirada() {
+        return data_retirada;
+    }
 
     @Override
     public String toString() {
         // alterar impleentação
-        return super.toString();
+        String dados_revista = "\n * Título: " + titulo + "\n * Edição: " + numero_edicao + "\n * Publicação: "
+                + mes_publicacao + "/" + ano_publicacao + "\n * Data cadastro: " + this.data_cadastro;
+        if (numero_volume > 0) {
+            dados_revista += "\n * Volume: " + numero_volume;
+        }
+        return dados_revista;
     }
-
 
 }
