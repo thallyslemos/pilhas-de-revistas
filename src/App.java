dd30180defc5478d26d14d75cc4f9df7f0e039d6
thallyslemos <thallysvilemos@gmail.com>
2023-06-16 01:46:51 -0300

    metodos de adição e remoção de mercadoria e exibição de nota

--- src/App.java
+++ src/App.java
@@ -1,11 +1,16 @@
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class App {
 
     public static void menuPrincipal() {
+        System.out.print("\033[H\033[2J");
         System.out.println("-------- Menu --------\n");
         System.out.println("(1) - Pilha de revistas");
         System.out.println("(2) - Vendas");
@@ -15,6 +20,7 @@ public class App {
     }
 
     public static void menuRevistas() {
+        System.out.print("\033[H\033[2J");
         System.out.println("------- Pilha de Revistas -------\n");
         System.out.println("(1) - Empilhar nova revista");
         System.out.println("(2) - Consultar próxima revista da pilha");
@@ -27,12 +33,49 @@ public class App {
     }
 
     public static void menuVenda() {
+        System.out.print("\033[H\033[2J");
         System.out.println("--------- Vendas ---------\n");
         System.out.println("(1) - Listar todas as vendas");
         System.out.println("(2) - Incluir nova venda");
         System.out.println("(3) - Limpar terminal");
         System.out.println("(0) - Voltar ao menu principal\n");
         System.out.print("Escolha uma opção: ");
+    }
+
+    public static void menuRealizaVenda() {
+        System.out.println("\n-----------------------------------------");
+        System.out.println("(1) - Incluir novo item");
+        System.out.println("(2) - Excluir item");
+        System.out.println("(0) - Finalizar venda\n");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    public static Integer buscarChavePorValor(Map<Integer, Mercadoria> mapa, Mercadoria valor) {
+        return mapa.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().equals(valor))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static void notaCompra(Map<Mercadoria, Integer> lista_compra, Map<Integer, Mercadoria> estoque) {
+                    System.out.printf("%-7s| %-35s| %-14s| %-11s| %-13s%n", "Código","Nome", "Preço Unitário", "Quantidade", "Valor Total");
+        System.out.println("----------------------------------------------------------------------------------------------");
+        double valor_total_compra = 0;
+        for (Map.Entry<Mercadoria, Integer> entry : lista_compra.entrySet()) {
+            Mercadoria mercadoria = entry.getKey();
+            int quantidade = entry.getValue();
+            double preco_unitario = mercadoria.getPreco();
+            double valor_total_item = quantidade * preco_unitario;
+            valor_total_compra += valor_total_item;
+            Integer id = buscarChavePorValor(estoque, mercadoria);
+
+            System.out.printf("%-7d| %-35s| R$ %-11.2f| %-11d| R$ %-11.2f%n",
+                   id, mercadoria.getNome(), preco_unitario, quantidade, valor_total_item);
+        }
+        // Adcionar itens
+        System.out.println("\nValor Total: " + String.format("R$ %.2f",valor_total_compra)); // Adcionar valor total
     }
 
     public static void revistas_por_ano(Stack<Revista> pilha_revistas, int ano) {
@@ -45,7 +88,7 @@ public class App {
             Revista revista = pilha_temporaria.pop();
             if (revista.getAno_publicacao() == ano) {
                 System.out.println(revista.toString());
-            } 
+            }
         }
         // while (!pilha_revistas.isEmpty()) {
         // Revista revista = pilha_revistas.pop();
@@ -64,6 +107,7 @@ public class App {
     }
 
     public static void cadastraRevista(Scanner scanner, Stack<Revista> pilha_revistas) {
+        System.out.print("\033[H\033[2J");
         System.out.println("Digite o título: ");
         String titulo = scanner.next();
         System.out.println("Digite número de edição: ");
@@ -113,6 +157,54 @@ public class App {
         System.out.println("Diferença de tempo: " + diffTime);
     }
 
+    public static void cria_estoque(Map<Integer, Mercadoria> estoque) {
+        estoque.put(1, new Mercadoria("Açúcar Refinado 1Kg", 2.15));
+        estoque.put(2, new Mercadoria("Detergente Líquido Incolor 500ml", 1.39));
+        estoque.put(3, new Mercadoria("Nuggets Tradicional 300g", 5.98));
+        estoque.put(4, new Mercadoria("Água Sanitária 2L", 3.98));
+        estoque.put(5, new Mercadoria("Extrato de Tomate 340g", 3.74));
+        estoque.put(6, new Mercadoria("Óleo de Soja 900ml", 3.10));
+        estoque.put(7, new Mercadoria("Amaciante 2L", 11.58));
+        estoque.put(8, new Mercadoria("Farinha de Trigo 1Kg", 2.28));
+        estoque.put(9, new Mercadoria("Pão de Forma Tradicional 500g", 5.85));
+        estoque.put(10, new Mercadoria("Arroz Branco 5Kg", 13.98));
+        estoque.put(11, new Mercadoria("Feijão 1Kg", 3.28));
+        estoque.put(12, new Mercadoria("Papel Higiênico 30m 4un", 6.45));
+        estoque.put(13, new Mercadoria("Azeite Tipo Extravirgem 500ml", 17.90));
+        estoque.put(14, new Mercadoria("Gim 750ml", 89.90));
+        estoque.put(15, new Mercadoria("Refrigerante 1,5L", 5.49));
+        estoque.put(16, new Mercadoria("Barra de Chocolate 135g", 5.99));
+        estoque.put(17, new Mercadoria("Hambúrguer de Frango e Boi 672g", 12.75));
+        estoque.put(18, new Mercadoria("Requeijão Cremoso Tradicional 200g", 2.99));
+        estoque.put(19, new Mercadoria("Bolacha Recheada de Chocolate 136g", 1.88));
+        estoque.put(20, new Mercadoria("Iogurte de Morango 40g 8un", 3.99));
+        estoque.put(21, new Mercadoria("Sabão em Pó 1Kg", 7.99));
+        estoque.put(22, new Mercadoria("Bombons Sortidos 300g", 7.98));
+        estoque.put(23, new Mercadoria("Lã de Aço 60g 8un", 1.86));
+        estoque.put(24, new Mercadoria("Sabonete 90g", 1.22));
+        estoque.put(25, new Mercadoria("Café em Pó Tradicional 500g", 11.88));
+        estoque.put(26, new Mercadoria("Leite Condensado 395g", 3.98));
+        estoque.put(27, new Mercadoria("Sal Refinado 1Kg", 2.19));
+        estoque.put(28, new Mercadoria("Cerveja 350ml", 2.39));
+        estoque.put(29, new Mercadoria("Leite UHT Integral 1L", 2.18));
+        estoque.put(30, new Mercadoria("Sorvete Napolitano 1,5L", 17.50));
+        estoque.put(31, new Mercadoria("Cerveja 500ml", 12.70));
+        estoque.put(32, new Mercadoria("Macarrão com Ovos Parafuso 500g", 2.65));
+        estoque.put(33, new Mercadoria("Suco Pronto para Consumo Néctar 1L", 4.92));
+        estoque.put(34, new Mercadoria("Creme de Avelã 350g", 17.90));
+        estoque.put(35, new Mercadoria("Macarrão Espaguete 500g", 6.49));
+        estoque.put(36, new Mercadoria("Uísque 12 Anos 750ml", 117.90));
+        estoque.put(37, new Mercadoria("Creme de Leite 200g", 2.49));
+        estoque.put(38, new Mercadoria("Maionese 500g", 4.98));
+        estoque.put(39, new Mercadoria("Vodca 998ml", 29.90));
+    }
+
+    public static void inserir_produto(Integer codigo_produto, Integer quantidade, Map<Integer, Mercadoria> estoque,
+            Map<Mercadoria, Integer> lista_compra) {
+        lista_compra.put(estoque.get(codigo_produto), quantidade);
+
+    }
+
     // recebe o historico de rebistas reitradas da pilha e exibe o tempo médio de
     // permanenciia
     // das revistas na pilha
@@ -139,26 +231,24 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         // Lista para manipulação dos itens da compra
-        // List<Mercadoria> lista_compra = new ArrayList<>();
+        Map<Mercadoria, Integer> lista_compra = new HashMap<>();
         // Implementação de pilha de revistas com arrayDeque
         Stack<Revista> pilha_revistas = new Stack<>();
-
         Stack<Revista> historico_revistas = new Stack<>();
+        Map<Integer, Mercadoria> estoque = new HashMap<>();
+
+        cria_estoque(estoque);
 
         int menu = 1;
 
         while (menu != 0) {
-            // System.out.print("\033[H\033[2J");
             menuPrincipal();
             menu = scanner.nextInt();
 
             switch (menu) {
-
                 case 1:
                     int segundo_menu = 1;
-
                     while (segundo_menu != 0) {
-                        // System.out.print("\033[H\033[2J");
                         menuRevistas();
 
                         segundo_menu = scanner.nextInt();
@@ -226,27 +316,33 @@ public class App {
                                 break;
                             case 2:
                                 int quarto_menu = 1;
+
                                 while (quarto_menu != 0) {
                                     // System.out.print("\033[H\033[2J");
-                                    // adcionar o número da venda
-                                    System.out.println("Nº da venda: ");
-                                    System.out.println("-----------------------------------------\n");
-                                    System.out.println("Itens: ");
-                                    // Adcionar itens
-                                    System.out.println("Valor Total: "); // Adcionar valor total
-                                    System.out.println("\n-----------------------------------------");
-                                    System.out.println("(1) - Incluir novo item");
-                                    System.out.println("(2) - Excluir item");
-                                    System.out.println("(0) - Finalizar venda\n");
-
-                                    System.out.print("Escolha uma opção: ");
+                                    notaCompra(lista_compra, estoque); // adcionar o número da venda
+                                    menuRealizaVenda();
                                     quarto_menu = scanner.nextInt();
 
                                     switch (quarto_menu) {
                                         case 1:
+                                            System.out.println("Digite o código do produto a ser adicionado: ");
+                                            int id_produto = scanner.nextInt();
+                                            System.out.println("Digite a quantidade: ");
+                                            int qnt_produto = scanner.nextInt();
+                                            inserir_produto(id_produto, qnt_produto, estoque, lista_compra);
+
                                             break;
-                                        case 3:
+                                        case 2:
                                             System.out.print("\033[H\033[2J");
+                                            System.out.println("Digite o número do produto na lista a ser excluido: ");
+                                            int n_produto = scanner.nextInt();
+                                            Mercadoria produto = estoque.get(n_produto);
+                                            System.out.println(lista_compra.remove(produto));
+                                            // for(Mercadoria m : lista_compra.keySet()){
+                                            // if(m.getNome() == produto.getNome()) {
+
+                                            // }
+                                            // }
                                             break;
                                         case 0:
                                             break;
--- src/Mercadoria.java
+++ src/Mercadoria.java
@@ -5,7 +5,7 @@ public class Mercadoria {
     public Mercadoria(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
-    } 
+    }
 
     public String getNome() {
         return nome;
@@ -17,7 +17,9 @@ public class Mercadoria {
 
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        return super.toString();
+        String nomeFormatado = String.format("%-40s", nome);
+        String precoFormatado = String.format("R$ %.2f", preco);
+        String linhaFormatada = nomeFormatado + " | " + String.format("%-10s", precoFormatado);
+        return linhaFormatada;
     }
 }
